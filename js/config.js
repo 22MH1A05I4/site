7af99d05ef6f1e8ca80d620061eb4c0149636712
22MH1A05I4 <119779629+22MH1A05I4@users.noreply.github.com>
2025-06-21 20:10:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_8Mpe4iPLu', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7s34ehitut5cn3jpenved5ubi8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_eEAwGK0zF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4ls6u9c6509dp6us8j3bk7q6gi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
